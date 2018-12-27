9950544d2ab2096966a4983696e8cdbbd75050f7
0aqz0 <18026332256@163.com>
2018-12-28 01:13:41 +0800

    test pass

--- rm_gym/envs/rm_env.py
+++ rm_gym/envs/rm_env.py
@@ -1,25 +1,68 @@
 #!/usr/bin/env python
-
 """
-
+RoboMaster AI Challenge Simulation Environment.
 """
 import gym
+from rm_gym.envs.map_view_2d import MapView2D
+import time
 
-class RoboMaster(gym.Env):
+
+class RoboMasterEnv(gym.Env):
+    """
+    The RoboMaster Simulation Class.
+    """
+
+    action_space = ["U", "D", "L", "R"]
+    observation_space = None
+
     def __init__(self):
-        pass
+        self.map_view = MapView2D()
+        self.state = None
 
     def step(self, action):
-        pass
+        """
+        Accepts an action and returns a tuple (observation, reward, done, info).
+        Args:
+            action (object): an action provided by the environment
+        Returns:
+            observation (object): agent's observation of the current environment
+            reward (float) : amount of reward returned after previous action
+            done (boolean): whether the episode has ended, in which case further step() calls will return undefined results
+            info (dict): contains auxiliary diagnostic information (helpful for debugging, and sometimes learning)
+        """
+        if action == 'R':
+            self.map_view.update([100,30])
+        elif action == 'D':
+            self.map_view.update([30,100])
+        elif action == 'L':
+            self.map_view.update([100,100])
+        else:
+            self.map_view.update()
+        done = False
+        reward = 0
+        info = {}
+        return self.state, reward, done, info
 
     def reset(self):
+        """
+        Resets the state of the environment and returns an initial observation.
+        Returns: observation (object): the initial observation of the
+            space.
+        """
         pass
 
     def render(self, mode='human'):
-        pass
+        """
+        Renders the environment.
+        """
+        return self.map_view.update()
 
-    def close(self):
-        pass
 
-    def seed(self, seed=None):
-        pass
+if __name__ == '__main__':
+    env = RoboMasterEnv()
+    env.reset()
+    while True:
+        for i in range(4):
+            env.render()
+            env.step(env.action_space[i])
+            time.sleep(1)
