c5d1fc6d01ffd71b4b60b491b2ce216901328520
0aqz0 <18026332256@163.com>
2018-12-27 21:13:48 +0800

    add rm_env.py

---
+++ rm_gym/envs/rm_env.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+"""
+
+"""
+import gym
+
+class RoboMaster(gym.Env):
+    def __init__(self):
+        pass
+
+    def step(self, action):
+        pass
+
+    def reset(self):
+        pass
+
+    def render(self, mode='human'):
+        pass
+
+    def close(self):
+        pass
+
+    def seed(self, seed=None):
+        pass
