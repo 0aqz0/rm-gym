6c64c8753828835240d74c0a0d18293125116fe0
0aqz0 <18026332256@163.com>
2018-12-30 04:23:05 +0800

    fix offset problem

--- rm_gym/envs/map_view_2d.py
+++ rm_gym/envs/map_view_2d.py
@@ -4,9 +4,12 @@ from pygame.locals import *
 background_image_filename = 'images/map.png'
 robot_image_filename = 'images/robot.png'
 MAP_SIZE = (800, 500)
+IMAGE_OFFSET = 30
 
 class MapView2D:
-
+    """
+    The Map View Class.
+    """
     def __init__(self, map_name="RoboMaster Simulation"):
         pygame.init()
         pygame.display.set_caption(map_name)
@@ -19,30 +22,31 @@ class MapView2D:
         self.robot2 = pygame.image.load(robot_image_filename).convert_alpha()
         self.robot3 = pygame.image.load(robot_image_filename).convert_alpha()
         self.robot4 = pygame.image.load(robot_image_filename).convert_alpha()
-        if __name__ == '__main__':     # weird
-            while True:
-                for event in pygame.event.get():
-                    if event.type == QUIT:
-                        exit()
-                self.update()
+        # if __name__ == '__main__':     # weird
+        #     while True:
+        #         for event in pygame.event.get():
+        #             if event.type == QUIT:
+        #                 exit()
+        #         self.update()
 
-    def update(self, robot1Pos=[30,30], robot2Pos=[700,30], robot3Pos=[30,400], robot4Pos=[700,400]):
+    def update(self, robot1Pos, robot2Pos, robot3Pos, robot4Pos):
         for event in pygame.event.get():
             if event.type == QUIT:
                 exit()
         # 绘制背景
         self.screen.blit(self.background, (0, 0))
         # 绘制小车
-        self.screen.blit(pygame.transform.rotate(self.robot1, 0), robot1Pos)
-        self.screen.blit(pygame.transform.rotate(self.robot2, 0), robot2Pos)
-        self.screen.blit(pygame.transform.rotate(self.robot3, 0), robot3Pos)
-        self.screen.blit(pygame.transform.rotate(self.robot4, 0), robot4Pos)
+        self.screen.blit(pygame.transform.rotate(self.robot1, 0), [robot1Pos[0]-IMAGE_OFFSET, robot1Pos[1]-IMAGE_OFFSET])
+        self.screen.blit(pygame.transform.rotate(self.robot2, 0), [robot2Pos[0]-IMAGE_OFFSET, robot2Pos[1]-IMAGE_OFFSET])
+        self.screen.blit(pygame.transform.rotate(self.robot3, 0), [robot3Pos[0]-IMAGE_OFFSET, robot3Pos[1]-IMAGE_OFFSET])
+        self.screen.blit(pygame.transform.rotate(self.robot4, 0), [robot4Pos[0]-IMAGE_OFFSET, robot4Pos[1]-IMAGE_OFFSET])
         # 刷新画面
         pygame.display.update()
 
     def quit_game(self):
         pygame.display.quit()
         pygame.quit()
+
 
 if __name__ == '__main__':
     MapView2D()
