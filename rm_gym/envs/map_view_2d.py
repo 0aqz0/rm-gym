8280c3ad6a3683c68ba99ecad9a69565b60e1ef4
0aqz0 <18026332256@163.com>
2018-12-30 15:03:35 +0800

    change move_robot func, add update func

--- rm_gym/envs/map_view_2d.py
+++ rm_gym/envs/map_view_2d.py
@@ -23,6 +23,7 @@ class MapView2D:
         self.robot3 = pygame.image.load(robot_image_filename).convert_alpha()
         self.robot4 = pygame.image.load(robot_image_filename).convert_alpha()
         # initialize the position of robots
+        self.robotInitialPos = [robot1Pos, robot2Pos, robot3Pos, robot4Pos]  # used in reset
         self.robot1Pos = robot1Pos
         self.robot2Pos = robot2Pos
         self.robot3Pos = robot3Pos
@@ -33,30 +34,44 @@ class MapView2D:
         move robots in the GUI
         :return:
         """
+        if robot1Pos is not None:
+            self.robot1Pos = robot1Pos
+        if robot2Pos is not None:
+            self.robot2Pos = robot2Pos
+        if robot3Pos is not None:
+            self.robot3Pos = robot3Pos
+        if robot4Pos is not None:
+            self.robot4Pos = robot4Pos
+        self.update()
+
+    def quit_game(self):
+        pygame.display.quit()
+        pygame.quit()
+
+    def reset_robot(self):
+        """
+        reset robots' position
+        :return: None
+        """
+        self.robot1Pos = self.robotInitialPos[0]
+        self.robot2Pos = self.robotInitialPos[1]
+        self.robot3Pos = self.robotInitialPos[2]
+        self.robot4Pos = self.robotInitialPos[3]
+
+    def update(self):
         for event in pygame.event.get():
             if event.type == QUIT:
                 exit()
         # 绘制背景
         self.screen.blit(self.background, (0, 0))
         # 绘制小车
-        if robot1Pos is not None:
-            self.robot1Pos = robot1Pos
         self.screen.blit(pygame.transform.rotate(self.robot1, 0), [self.robot1Pos[0]-IMAGE_OFFSET, self.robot1Pos[1]-IMAGE_OFFSET])
-        if robot2Pos is not None:
-            self.robot2Pos = robot2Pos
         self.screen.blit(pygame.transform.rotate(self.robot2, 0), [self.robot2Pos[0]-IMAGE_OFFSET, self.robot2Pos[1]-IMAGE_OFFSET])
-        if robot3Pos is not None:
-            self.robot3Pos = robot3Pos
         self.screen.blit(pygame.transform.rotate(self.robot3, 0), [self.robot3Pos[0]-IMAGE_OFFSET, self.robot3Pos[1]-IMAGE_OFFSET])
-        if robot4Pos is not None:
-            self.robot4Pos = robot4Pos
         self.screen.blit(pygame.transform.rotate(self.robot4, 0), [self.robot4Pos[0]-IMAGE_OFFSET, self.robot4Pos[1]-IMAGE_OFFSET])
         # 刷新画面
         pygame.display.update()
 
-    def quit_game(self):
-        pygame.display.quit()
-        pygame.quit()
 
 
 if __name__ == '__main__':
